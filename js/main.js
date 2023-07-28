6e2431903b57aea94b48f8bfb729c5dd2baee9a6
ErauQsPhan <mrsplol102@gmail.com>
2023-07-28 15:07:58 +0700

    add scroll top

--- js/main.js
+++ js/main.js
@@ -1,22 +1,33 @@
-document.addEventListener("DOMContentLoaded", function() {
-   // Mã JavaScript của bạn ở đây
-   var toggleButton = document.getElementById('toggle-button');
-   toggleButton.addEventListener('click', toggleMode);
-   const moonIcon = document.getElementById("moon-icon");
+document.addEventListener("DOMContentLoaded", function () {
+  // Mã JavaScript của bạn ở đây
+  var toggleButton = document.getElementById("toggle-button");
+  toggleButton.addEventListener("click", toggleMode);
+  const moonIcon = document.getElementById("moon-icon");
 
+  // Hàm thay đổi chế độ
+  function toggleMode() {
+    var body = document.body;
+    body.classList.toggle("dark-mode");
 
-   // Hàm thay đổi chế độ
- function toggleMode() {
-   var body = document.body;
-   body.classList.toggle('dark-mode');
-
-   if (moonIcon.classList.contains("fa-moon")) {
-    moonIcon.classList.remove("fa-moon");
-    moonIcon.classList.add("fa-sun");
-  } else {
-    moonIcon.classList.remove("fa-sun");
-    moonIcon.classList.add("fa-moon");
+    if (moonIcon.classList.contains("fa-moon")) {
+      moonIcon.classList.remove("fa-moon");
+      moonIcon.classList.add("fa-sun");
+    } else {
+      moonIcon.classList.remove("fa-sun");
+      moonIcon.classList.add("fa-moon");
+    }
   }
-}
 });
 
+function scrollTop() {
+  const scrollBtn = document.querySelector("#scroll-top-btn");
+  if (this.scrollY > 300) {
+    scrollBtn.classList.add("d-flex");
+    scrollBtn.classList.remove("d-none");
+  } else {
+    scrollBtn.classList.add("d-none");
+    scrollBtn.classList.remove("d-flex");
+  }
+}
+
+window.addEventListener("scroll", scrollTop);
